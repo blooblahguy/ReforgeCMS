daafb1b4dad2f38595d93c2306f1f3b4162e551d
Glade Lynch <blooblahguy@gmail.com>
2020-07-09 15:26:28 -0600

    deleting old files

--- core/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// phpinfo();
-
-chdir("/home/runcloud/webapps/bigdumb/");
-echo exec("git pull git@github.com:blooblahguy/ReforgeCMS.git 2>&1", $output);
-
-// echo "test";
-print_r($output);

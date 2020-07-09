651c38ad76c43cd36dd5e987f12b91a54d52484a
Glade Lynch <blooblahguy@gmail.com>
2020-07-09 13:57:52 -0600

    again

--- core/deploy.php
+++ core/deploy.php
@@ -3,7 +3,7 @@
 define("TOKEN", "BDGSECRET_2020_GLADE");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@github.com:blooblahguy/ReforgeCMS.git"); // The SSH URL to your repository
 define("DIR", "/home/runcloud/webapps/bigdumb/");                       // The path to your repostiroy; this must begin with a forward slash (/)
-define("BRANCH", "refs/head/master");                             				// The branch route
+define("BRANCH", "refs/heads/master");                             				// The branch route
 define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/lib/git-core");                                    // The path to the git executable
 define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)

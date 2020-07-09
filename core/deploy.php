56f2cf68b0e162527f25d2264783d4a169d4d9fe
blooblahguy <blooblahguy@gmail.com>
2020-07-09 21:13:57 +0000

    fix

--- core/deploy.php
+++ core/deploy.php
@@ -4,8 +4,7 @@ $content = file_get_contents("php://input");
 $secret = "BDGSECRET_2020_GLADE";
 $token = false;
 $json = json_decode($content, true);
-
-// $id = $json['repository']['id'];
+$id = $json['repository']['id'];
 
 // if ($id != 242001243) {
 // 	http_response_code(403);
@@ -13,21 +12,14 @@ $json = json_decode($content, true);
 // }
 
 chdir("/home/runcloud/webapps/bigdumb/");
-echo exec("git pull git@github.com:blooblahguy/ReforgeCMS.git 2>&1", $output, $exit);
+exec("git pull git@github.com:blooblahguy/ReforgeCMS.git 2>&1", $output, $exit);
+
+if ($exit !== 0) {
+	http_response_code(403);
+	print_r($output);
+	exit();
+}
 
 echo "<pre>";
 print_r($output);
-print_r($exit);
 echo "</pre>";
-
-// define("TOKEN", "BDGSECRET_2020_GLADE");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
-// define("REMOTE_REPOSITORY", "git@github.com:blooblahguy/ReforgeCMS.git"); // The SSH URL to your repository
-// define("DIR", "/home/runcloud/webapps/bigdumb/");                       // The path to your repostiroy; this must begin with a forward slash (/)
-// define("BRANCH", "refs/heads/master");                             				// The branch route
-// define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
-// define("GIT", "git");    												 // The path to the git executable
-// define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
-// define("BEFORE_PULL", "");                                             // A command to execute before pulling
-// define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
-
-// include "deployer.php";

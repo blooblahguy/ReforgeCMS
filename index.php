c460f98c781f55127526ccb80405e4eda7b72830
Glade Lynch <blooblahguy@gmail.com>
2020-07-09 22:54:27 -0600

    recommit

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?
 
-// phpinfo();
-// exit();
-
-// Call this at each point of interest, passing a descriptive string
-// function prof_flag($str) {
-//     global $prof_timing, $prof_names;
-//     $prof_timing[] = microtime(true);
-//     $prof_names[] = $str;
-// }
-
-// // Call this when you're done and want to see the results
-// function prof_print() {
-//     global $prof_timing, $prof_names;
-//     $size = count($prof_timing);
-//     for($i=0;$i<$size - 1; $i++)
-//     {
-//         echo "<b>{$prof_names[$i]}</b><br>";
-//         echo sprintf("&nbsp;&nbsp;&nbsp;%f<br>", $prof_timing[$i+1]-$prof_timing[$i]);
-//     }
-//     echo "<b>{$prof_names[$size-1]}</b><br>";
-// }
-
-
-
-
 $PATH = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");	
 list($CONTROLLER) = explode("/", $PATH);
 $root = rtrim($_SERVER['DOCUMENT_ROOT'], "/");

2089dd72f5ed7e70248646e7e9f5e1c288fde294
Glade Lynch <blooblahguy@gmail.com>
2020-07-09 22:53:04 -0600

    adding error log

--- index.php
+++ index.php
@@ -23,12 +23,15 @@
 // }
 
 
-error_reporting(E_ALL);
-// ini_set("display_errors", 1);
+
 
 $PATH = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");	
 list($CONTROLLER) = explode("/", $PATH);
 $root = rtrim($_SERVER['DOCUMENT_ROOT'], "/");
+
+error_reporting(E_ALL);
+ini_set("error_log", $root."/core/logs/php-error-".Date("Y-m-d").".log");
+ini_set("display_errors", 1);
 
 require $root."/core/init.php";
 require $root."/core/core.php";

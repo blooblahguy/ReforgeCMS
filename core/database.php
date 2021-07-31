d44bc265d00ea897b7bcd8aa2963f4f815520428
blooblahguy <blooblahguy@gmail.com>
2021-07-30 18:26:50 -0600

    different cron connection

--- core/database.php
+++ core/database.php
@@ -1,13 +1,21 @@
 <?php
 
 // Database
-$db = new DB\SQL(
-	"mysql:host={$configuration["database_host"]};port={$configuration["database_port"]};dbname={$configuration["database"]}",
-	"{$configuration["database_user"]}",
-	"{$configuration["database_password"]}",
-	array(
-		\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-		\PDO::ATTR_PERSISTENT => true,
-		\PDO::MYSQL_ATTR_COMPRESS => true,
-	)
-);
+if ($cron) {
+	$db = new DB\SQL(
+		"mysql:host={$configuration["database_host"]};port={$configuration["database_port"]};dbname={$configuration["database"]}",
+		"{$configuration["database_user"]}",
+		"{$configuration["database_password"]}",
+	);
+} else {
+	$db = new DB\SQL(
+		"mysql:host={$configuration["database_host"]};port={$configuration["database_port"]};dbname={$configuration["database"]}",
+		"{$configuration["database_user"]}",
+		"{$configuration["database_password"]}",
+		array(
+			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+			\PDO::ATTR_PERSISTENT => true,
+			\PDO::MYSQL_ATTR_COMPRESS => true,
+		)
+	);
+}
